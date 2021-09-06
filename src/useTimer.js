05e7cd8b70b8e59f9e224d0fedb9364d0bb76940
Suncerie Daye <sunceried@gmail.com>
2021-09-06 02:58:32 -0700

    useTimer custom hook: start functionality begun

--- src/useTimer.js
+++ src/useTimer.js
@@ -1,8 +1,20 @@
+import {useState} from 'react';
+
 const useTimer = (totalDuraton) => {
+    const [seconds, setSeconds] = useState(totalDuraton);
     const isRunning = false;
-    const start = () => {};
+    const start = () => {
+        /*
+            Start timer
+            decremenet seconds ever 1s
+            update the isRunning !!! true 
+         */
+        setInterval(() =>{
+            setSeconds(seconds-1); //Need to countdown to zero. This will cause a rerender so this will happen every second.
+        }, 1000); //will be called after one sec
+    };
     const stop = () => {};
-    const seconds = totalDuraton; 
+    
     return {isRunning, start, stop, seconds};
 }
 
